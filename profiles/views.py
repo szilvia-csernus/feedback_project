ca380d1bbd742ea507e48a904d186ef15f9fc10f
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-03 21:06:04 +0100

    add file upload functionality

---
+++ profiles/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+
+
+def store_file(file):
+    with open('temp/image.jpg', 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, 'profiles/create_profile.html')
+    
+    def post(self, request):
+        store_file(request.FILES['image'])
+        return HttpResponseRedirect('/profiles')
