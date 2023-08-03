1f0daf49694ac289ab44a5dcefad2e0d42ae08f8
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-03 19:34:19 +0100

    add list and detail views

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ReviewView.as_view()),
-    path('thank_you', views.thank_you)
+    path('thank_you', views.ThankYouView.as_view()),
+    path('reviews', views.ReviewListView.as_view()),
+    path('reviews/<int:pk>', views.SingleReview.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,12 @@
+from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from .models import Review
 
 class ReviewView(View):
     def get(self, request):
@@ -42,5 +47,27 @@ class ReviewView(View):
 #     })
 
 
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
+class ThankYouView(TemplateView):
+    template_name = 'reviews/thank_you.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'This works!'
+        return context
+    
+    
+class ReviewListView(ListView):
+    template_name = 'reviews/review_list.html'
+    model = Review
+    context_object_name = 'reviews'
+    
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=3)
+    #     return data
+    
+    
+class SingleReview(DetailView):
+    template_name = 'reviews/single_review.html'
+    model = Review
+    context_object_name = 'review'
