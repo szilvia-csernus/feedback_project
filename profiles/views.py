07891516c8ac9810a173b350a31928ef01f28da8
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-08-04 08:24:28 +0100

    add file upload functionality with CreateView

--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,36 @@
-from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponseRedirect
+# from django.shortcuts import render
+# from django.views import View
+# from django.http import HttpResponseRedirect
+from django.views.generic import CreateView
+
+# from .forms import ProfileForm
+from .models import UserProfile
 
 
-def store_file(file):
-    with open('temp/image.jpg', 'wb+') as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
+class CreateProfileView(CreateView):
+    template_name = 'profiles/create_profile.html'
+    model = UserProfile
+    fields = "__all__"
+    success_url = '/profiles'
 
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, 'profiles/create_profile.html')
+# def store_file(file):
+#     with open('temp/image.jpg', 'wb+') as destination:
+#         for chunk in file.chunks():
+#             destination.write(chunk)
+
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, 'profiles/create_profile.html', {'form': form})
     
-    def post(self, request):
-        store_file(request.FILES['image'])
-        return HttpResponseRedirect('/profiles')
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+        
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES['user_image'])
+#             profile.save()
+#             return HttpResponseRedirect('/profiles')
+        
+#         return render(request, 'profiles/create_profile.html', {
+#             'form': submitted_form
+#         })
